43c496c1f673cd07c8970b063f25d0fdd04fad5a
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-08-08 19:27:17 +0600

    navbar -- Mahmud

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Main from './layouts/Main'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <Main></Main>
+    </RouterProvider>
   </React.StrictMode>,
 )
---
+++ src/routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>
+    }
+])
+
