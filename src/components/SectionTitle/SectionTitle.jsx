8e8e596960e544fd56b5aa392e44a6ed418d6920
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-08-09 00:46:19 +0600

    footer initiated section title edit

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,8 +1,8 @@
 
-const SectionTitle = ({title,subTitle}) => {
+const SectionTitle = ({ title, subTitle }) => {
     return (
         <div className="text-center">
-            <h2 className="font-bold text-2xl">{title}</h2>
+            <h2 className="font-bold text-3xl mb-2 inline-block px-5 pb-1 border-b-[2px] border-green-400">{title}</h2>
             <p className="">{subTitle}</p>
         </div>
     );
---
+++ src/pages/shared/Footer.jsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+
+const Footer = () => {
+    return (
+        <div className="bg-green-50 min-h-[50vh] rounded-t-3xl">
+
+        </div>
+    );
+};
+
+export default Footer;
