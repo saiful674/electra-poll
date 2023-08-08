879b0bc05515e7de32eb2c24f801258bb8fc9fdd
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-08-08 03:56:11 -0700

    router, layout and project folder created

---
+++ src/layout/Main.jsx
@@ -0,0 +1,11 @@
+import Home from "../pages/HomePage/Home/Home";
+
+const Main = () => {
+    return (
+        <div>
+            <Home/>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/HomePage/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h3>Hello world</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nemo, rem reiciendis velit voluptatum excepturi dolores corrupti et iusto voluptas quod? Incidunt iure ipsam exercitationem dicta ipsa doloribus porro harum.</p>
+        </div>
+    );
+};
+
+export default Home;
