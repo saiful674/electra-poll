37a59f0406ca10684be0cd9176b8d493d439b793
hridoy281810 <hridoy01724281810@gmail.com>
2023-08-09 16:01:50 +0600

    faq inisialize - anisur

---
+++ src/pages/HomePage/Faq/Faq.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const Faq = () => {
+    return (
+        <div>
+            <SectionTitle title={"Frequently Asked Question"} ></SectionTitle>
+        </div>
+    );
+};
+
+export default Faq;
