39bd48d21dec58104edc60c35b07ca96dc030161
Farukul Islam <farukgb1999@gmail.com>
2023-08-14 17:12:38 +0600

    about page maked by farukul islam

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,23 +3,28 @@ import Main from "../layout/Main";
 import Home from "../pages/HomePage/Home/Home";
 import Service from "../pages/Service/Service";
 import ErrorPage from "../pages/shared/ErrorPage";
+import AboutPage from "../pages/AboutPage/AboutPage";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
         path: "/",
-        element: <Main></Main>,
-        errorElement: <ErrorPage></ErrorPage>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: 'services',
-                element: <Service></Service>
-            }
-        ]
-    }
-])
+        element: <Home></Home>,
+      },
+      {
+        path: "services",
+        element: <Service></Service>,
+      },
+      {
+        path: "about",
+        element: <AboutPage></AboutPage>,
+      },
+    ],
+  },
+]);
 
 export default router;
