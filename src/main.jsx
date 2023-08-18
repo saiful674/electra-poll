3200dae36a871288491d1db7b2ac08e98112b122
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-08-17 22:34:32 -0700

    add voter function and voters show in a tabulor form ui designed

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,25 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/routes'
-import Main from './layout/Main'
-import AuthProviders from './Providers/AuthProvider'
 import { Provider } from 'react-redux'
+import { RouterProvider } from 'react-router-dom'
+import AuthProviders from './Providers/AuthProvider'
+import './index.css'
+import Main from './layout/Main'
 import { store } from './redux/store/Store'
+import router from './routes/routes'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <AuthProviders>
+      <QueryClientProvider client={queryClient}>
         <RouterProvider router={router}>
           <Main></Main>
         </RouterProvider>
+        </QueryClientProvider>
       </AuthProviders>
     </Provider>
   </React.StrictMode>
