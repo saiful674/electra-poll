4bf3c71abcda8347d81ccadab6875a3a8a7298ee
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-08-19 01:29:21 -0700

    get voters api issue fixed

---
+++ src/components/ChatBot/ChatBot.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatBot = () => {
+    return (
+        <div>
+            <iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/4732a2e4-0eb9-4cd5-8cd9-434d31adf65c"></iframe>
+
+        </div>
+    );
+};
+
+export default ChatBot;
