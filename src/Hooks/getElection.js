b8f2839d50cc4f4dc28dd4f185cd6c88bdfb51e6
hridoy281810 <hridoy01724281810@gmail.com>
2023-08-20 16:37:14 +0600

    dynamic toatal electosn and total voters  by-  Mohammad  Aanisur Rahman

--- src/Hooks/getElection.js
+++ src/Hooks/getElection.js
@@ -18,18 +18,3 @@ const { data: elections = [], refetch} = useQuery({
 export default getElection;
 
 
-
-// const useMySelected = () => {
-//   const { user, loading } = useAuth()
-//   const { refetch, data: mySelected = [] } = useQuery({
-//     queryKey: ['mySelected', user?.email],
-//     enabled: !loading,
-//     queryFn: async () => {
-//       const res = await fetch(`http://localhost:5000/selected?email=${user?.email}`)
-//       return res.json()
-//     }
-
-//   })
-//   return [mySelected, refetch]
-// };
-// export default useMySelected;
