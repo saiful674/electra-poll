912088af41081f106c7d633d59b501799d9e2758
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-08-28 22:46:12 +0600

    overview update

--- src/pages/Dashboard/SubPages/ElectionResult/ElectionResult.jsx
+++ src/pages/Dashboard/SubPages/ElectionResult/ElectionResult.jsx
@@ -25,7 +25,7 @@ const ElectionResult = () => {
 
     const handleDownloadClick = async () => {
         try {
-            const response = await fetch('http://localhost:5173/download-election-data');
+            const response = await fetch('https://electra-poll-server.vercel.app/download-election-data');
             const blob = await response.blob();
 
             // Create a blob URL and simulate download
