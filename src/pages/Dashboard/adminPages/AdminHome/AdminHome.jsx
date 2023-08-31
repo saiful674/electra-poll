ffdb5e61990ace6c1b4788af4dfcbb4693b98914
hridoy281810 <hridoy01724281810@gmail.com>
2023-08-31 22:38:28 +0600

    admin home pase some work done  by-  Mohammad  Aanisur Rahman

--- src/pages/Dashboard/adminPages/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/adminPages/AdminHome/AdminHome.jsx
@@ -3,23 +3,49 @@ import AdminUserName from './AdminUserName';
 import ElectionHistory from './ElectionHistory';
 import TotalUser from './TotalUser';
 import { useState } from 'react';
+import LoadingSpinner from '../../../shared/LoadingSpinner';
+import BlogHistory from './BlogHistory';
 
 const AdminHome = () => {
     const [electionData, setElectionData] = useState([]);
-
+    const [blogData, setBlogData] = useState([]);
+    const [loading,setLoading] = useState(true)
     useEffect(() => {
+        setLoading(true)
       fetch('http://localhost:5000/all-elections')
         .then(response => response.json())
-        .then(data => setElectionData(data))
-        .catch(error => console.error('Error fetching data:', error));
+        .then(data => {
+            setElectionData(data)
+            setLoading(false)
+        })
+        .catch(error =>{ console.error('Error fetching data:', error)
+        setLoading(false)
+    });
     }, []);
-  
+    useEffect(()=> {
+        setLoading(true)
+        fetch(`http://localhost:5000/blogs`)
+        .then(res=> res.json())
+        .then(data =>{
+            console.log(data)
+            setBlogData(data)
+             setLoading(false)
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            setLoading(false); // There's an error, set loading to false
+          });
+    },[])
+    if(loading){
+        return <LoadingSpinner />
+    }
     return (
         <div >
             
         <AdminUserName />
          <TotalUser />
          <ElectionHistory electionData={electionData} />
+         <BlogHistory blogData={blogData} />
          
         </div>
     );
