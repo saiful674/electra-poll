ebcf8051e3fe03c6030a9f5efa59e6406c42cae4
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-09-10 17:09:18 +0600

    user review page polish and link update

--- src/pages/Dashboard/SubPages/UserReview/UserReview.jsx
+++ src/pages/Dashboard/SubPages/UserReview/UserReview.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from "../../../../Providers/AuthProvider";
 import axios from "axios";
 import toast from "react-hot-toast";
 import Swal from "sweetalert2";
-import ratingImg from "../../../../assets/rating.png";
+import ratingImg from "../../../../assets/favicon.png";
 import getMyInfo from "../../../../Hooks/getMyInfo";
 
 const UserReview = () => {
@@ -37,7 +37,7 @@ const UserReview = () => {
 
     if (rating) {
       axios
-        .post(`http://localhost:5000/user-review`, reviewData)
+        .post(`https://electra-poll-server.vercel.app/user-review`, reviewData)
         .then((response) => {
           if (response.data.insertedId) {
             Swal.fire("Thanks!", "Your feedback means a lot to us!", "success");
@@ -51,11 +51,27 @@ const UserReview = () => {
     }
   };
 
+  const CustomStar = (
+    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+  );
+
+  const myStyles = {
+    itemShapes: CustomStar,
+    itemStrokeWidth: 1,
+    activeFillColor: "#3ae895",
+    activeStrokeColor: "#3ae895",
+    inactiveStrokeColor: "#3ae895",
+  };
+
   return (
     <div>
       <UserName />
-      <div className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto my-10 text-center bg-gray-200 rounded-lg">
-        <img className="w-1/2 mx-auto" src={ratingImg} alt="" />
+      {/* <div className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto my-10 text-center bg-gray-200 rounded-lg"> */}
+      <div
+        style={{ border: "2px solid #3ae895" }}
+        className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto my-10 text-center shadow-md rounded px-8 pt-6 pb-8 border-green-500"
+      >
+        <img className="w-1/3 mx-auto mb-5" src={ratingImg} alt="" />
         <h3 className="text-2xl font-bold">Rate Our Website</h3>
         <hr className="border-slate-500 h-1 mx-10 my-5" />
         <form
@@ -66,6 +82,7 @@ const UserReview = () => {
             style={{ maxWidth: 180 }}
             value={rating}
             onChange={setRating}
+            itemStyles={myStyles}
             isRequired
           />
           <input
