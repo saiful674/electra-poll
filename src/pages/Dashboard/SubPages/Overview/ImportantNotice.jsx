be672060a75f57127959e417fe0cad2ad5a4ae4e
hridoy281810 <hridoy01724281810@gmail.com>
2023-09-13 16:03:27 +0600

    complete important notice and massege when data no found in userAdmin dashboard onverview page   by-  Mohammad  Aanisur Rahman

--- src/pages/Dashboard/SubPages/Overview/ImportantNotice.jsx
+++ src/pages/Dashboard/SubPages/Overview/ImportantNotice.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import MarqueeTitle from './MarqueeTitle';
+import { AiFillNotification } from 'react-icons/ai';
 
 const ImportantNotice = () => {
    
 
     return (
-        <div className='bg-red-200 mb-6 p-4 rounded shadow ' >
+        <div className='bg-red-200 mb-6 p-4 rounded shadow  overflow-x-hidden ' >
         <div className='flex items-center'>
                 <div>
           <div className='flex items-center'>
-          <AiOutlineExclamationCircle className='text-2xl text-red-600 mr-2' />
+          <AiFillNotification className='text-2xl text-red-600 mr-2' />
                     <h2 className='text-lg font-semibold mb-2 md:mb-0'>Important ImportantNotices or Updates</h2>
                    
           </div>
@@ -18,8 +19,8 @@ const ImportantNotice = () => {
            <p className='text-sm mb-2 md:mb-0'>
                         Stay informed about important security updates on our website.
                     </p>
-               <div>
-              
+               <div className='pt-2'>
+              <MarqueeTitle></MarqueeTitle>
                </div>
                 </div>
             </div>
