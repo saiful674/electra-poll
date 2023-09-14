3bfd272466a3cb587b75954945a47fd3f9a432fb
hridoy281810 <hridoy01724281810@gmail.com>
2023-09-14 09:04:14 +0600

    set nicely time and date shoing impotant notice on overview page   by-  Mohammad  Aanisur Rahman

--- src/pages/Dashboard/SubPages/Overview/MarqueeTitle.jsx
+++ src/pages/Dashboard/SubPages/Overview/MarqueeTitle.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import getElection from '../../../../Hooks/getElection';
 import { FaAngleRight } from 'react-icons/fa6';
 import Marquee from 'react-fast-marquee';
+import moment from 'moment'; 
 const MarqueeTitle = () => {
     const [elections] = getElection();
     return (
@@ -13,8 +14,8 @@ const MarqueeTitle = () => {
                               
                                     <div className='flex items-center gap-2'>
                                         <p>
-                                            The last time to vote in {election?.title} election is{' '}
-                                            {election?.endDate}
+                                            The last time to vote in "{election?.title}" election is{' '}
+                                            {moment(election?.endDate).format('MMMM Do YYYY, h:mm a')}
                                         </p>{' '}
                                         <FaAngleRight className='mr-2' />
                                     </div>
