63263e5cce488890eb2e65e1696456a91137fff4
hridoy281810 <hridoy01724281810@gmail.com>
2023-09-14 18:24:50 +0600

    set nicely time and date shoing impotant notice on overview page   by-  Mohammad  Aanisur Rahman

--- src/pages/Dashboard/SubPages/Overview/ImportantNotice.jsx
+++ src/pages/Dashboard/SubPages/Overview/ImportantNotice.jsx
@@ -3,27 +3,23 @@ import MarqueeTitle from './MarqueeTitle';
 import { AiFillNotification } from 'react-icons/ai';
 
 const ImportantNotice = () => {
-   
+
 
     return (
         <div className='bg-red-200 mb-6 p-4 rounded shadow  overflow-x-hidden ' >
-        <div className='flex items-center'>
-                <div>
-          <div className='flex items-center'>
-          <AiFillNotification className='text-2xl text-red-600 mr-2' />
+
+            <div>
+                <div className='flex items-center overflow-x-hidden'>
+                    <AiFillNotification className='text-2xl text-red-600 mr-2' />
                     <h2 className='text-xl font-semibold mb-2 md:mb-0'>Important ImportantNotices or Updates</h2>
-                   
-          </div>
-
-
-           <p className='text-md mb-2 md:mb-0'>
-                        Stay informed about important security updates on our website.
-                    </p>
-               <div className='pt-2'>
-              <MarqueeTitle></MarqueeTitle>
-               </div>
                 </div>
+                <p className='text-md mb-2  whitespace-normal'>
+                    Stay informed about important security updates on our website.
+                </p>
+                <MarqueeTitle></MarqueeTitle>
+
             </div>
+
         </div>
     );
 };
